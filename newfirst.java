4cd2c1f392887494634f397809d96cd09a1f4a88
chaithanya.b <chaithanya@tcs.com>
2023-02-04 08:35:33 +0000

    write the full story of the conflict developers involved : chaitu,kumar manager involved : chaithanya conflict resolved

--- newfirst.java
+++ newfirst.java
@@ -1,4 +1,4 @@
-written on master-branch... this is the first java program in tcs project...!!!
+this is the first java program in tcs project...the correct logic suggested by the manager...!!!
 
 
 adding secondline of code
